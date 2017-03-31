be0e5b7529913e2fd9bf24ed154bb37b14353957
Valentin Brosseau <c4software@gmail.com>
2017-03-31 21:56:48 +0200

    Update bundle and icone information

--- setup.py
+++ setup.py
@@ -1,16 +1,26 @@
 from setuptools import setup
 
+APP_NAME = "kwm-status-menu"
 APP = ['menu_status.py']
 DATA_FILES = []
 OPTIONS = {
     'argv_emulation': True,
     'plist': {
         'LSUIElement': True,
+        'CFBundleName': APP_NAME,
+        'CFBundleDisplayName': APP_NAME,
+        'CFBundleGetInfoString': "KWM Status Menu",
+        'CFBundleIdentifier': "com.vbrosseau.kwm-status-menu",
+        'CFBundleVersion': "0.4.0",
+        'CFBundleShortVersionString': "0.4.0",
+        'NSHumanReadableCopyright': u"Valentin Brosseau"
     },
+    'iconfile': 'app.icns',
     'packages': [],
 }
 
 setup(
+    name=APP_NAME,
     app=APP,
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
